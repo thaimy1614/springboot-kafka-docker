b8499afee0ea75ec5619936f71423459086e49dd
thaimy1614 <lxlthailxl@gmail.com>
2024-06-02 17:04:18 +0700

    create project

---
+++ emailservice/src/test/java/com/thai/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thai.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
